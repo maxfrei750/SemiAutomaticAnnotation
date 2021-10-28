2d679f17b3309a0f9dd690d8a0e35fdc65ad4b42
Max Frei <max.frei@uni-due.de>
2021-10-28 23:55:30 +0200

    Refactored frontend and make dash use frontend port set by env variable.

--- frontend.py
+++ frontend.py
@@ -3,48 +3,72 @@ import os
 import dash
 import plotly.express as px
 import plotly.io as pio
-from dash import dcc, html
+from dash import Input, Output, State, dcc, html
 
 from prediction import read_image
 
 pio.templates.default = "plotly_white"
 
-image_path = "/home/tensorflow/input/image3.jpg"
-image = read_image(image_path)
-
-fig = px.imshow(image)
-fig.update_layout(dragmode="drawrect")
-
-crosshair_kwargs = {
-    "showspikes": True,
-    "spikemode": "across",
-    "spikesnap": "cursor",
-    "spikethickness": 0.5,
-    "visible": False,
-}
-
-fig.update_xaxes(**crosshair_kwargs)
-fig.update_yaxes(**crosshair_kwargs)
-
-fig.update_traces(hoverinfo="none", hovertemplate="")
-
-graph_config = {
-    "displaylogo": False,
-    "displayModeBar": True,
-    "scrollZoom": True,
-    "modeBarButtons": [["eraseshape", "autoScale2d"]],
-}
+PORT_FRONTEND = int(os.environ["PORT_FRONTEND"])
 
 
-app = dash.Dash(__name__)
+def main():
+    image_path = "/home/tensorflow/input/image3.jpg"
+    image = read_image(image_path)
 
-app.layout = html.Div(
-    [
-        dcc.Graph(figure=fig, config=graph_config, style={"height": "100%"}),
-    ],
-    style={"height": "98vh"},
-)
+    figure = px.imshow(image)
+
+    style_annotations(figure)
+    style_cursor(figure)
+
+    app = dash.Dash(__name__)
+
+    graph_config = {
+        "displaylogo": False,
+        "displayModeBar": True,
+        "scrollZoom": True,
+        "modeBarButtons": [["eraseshape", "autoScale2d"]],
+    }
+
+    app.layout = html.Div(
+        [
+            dcc.Graph(figure=figure, config=graph_config, style={"height": "95%"}),
+            html.Center(html.Button("Submit", id="submit-button")),
+        ],
+        style={"height": "98vh"},
+    )
+
+    # @app.callback(Input("submit-button", "n_clicks"))
+    # def update_output(n_clicks, value):
+    #     print(
+    #         'The input value was "{}" and the button has been clicked {} times'.format(value, n_clicks)
+    #     )
+    #
+
+    app.run_server(host="0.0.0.0", port=PORT_FRONTEND, debug=True, dev_tools_ui=False)
+
+
+def style_cursor(figure):
+    # Style cursor
+    crosshair_kwargs = {
+        "showspikes": True,
+        "spikemode": "across",
+        "spikesnap": "cursor",
+        "spikethickness": 0.5,
+        "visible": False,
+    }
+    figure.update_xaxes(**crosshair_kwargs)
+    figure.update_yaxes(**crosshair_kwargs)
+    figure.update_traces(hoverinfo="none", hovertemplate="")
+
+
+def style_annotations(figure):
+    # Style annotation
+    figure.update_layout(
+        dragmode="drawrect",
+        newshape={"fillcolor": None, "opacity": 1.0, "line": {"color": "red", "width": 3}},
+    )
+
 
 if __name__ == "__main__":
-    print(f"Annotation tool is running on http://localhost:{os.environ['PORT_FRONTEND']}/")
-    app.run_server(host="0.0.0.0", debug=True)
+    main()
