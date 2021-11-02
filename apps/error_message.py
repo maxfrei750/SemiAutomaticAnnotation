40f897aca546acdd1c47a1810c42b7351502c53f
Max Frei <max.frei@uni-due.de>
2021-11-02 18:53:02 +0100

    Added parameter to add links to error messages.

--- apps/error_message.py
+++ apps/error_message.py
@@ -1,13 +1,20 @@
-from dash import html
+from typing import List, Optional
+
+from dash import dcc, html
 
 
-def get_layout(message: str):
+def get_layout(message: str, links: Optional[List[dcc.Link]] = None):
+
+    if links is None:
+        links = []
+
     layout = html.Div(
         [
             html.Center(
                 [
                     html.H1(message),
                 ]
+                + [html.H1(link) for link in links]
             )
         ],
         style={"height": "98vh"},
