86dee5746eab572dba95fb0eda0c142233d7ce1f
Max Frei <max.frei@uni-due.de>
2022-08-23 09:54:03 +0200

    Replaced demo with an improved gif.

--- assets/images/morph_images.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-convert -resize 600x600 image_D2_crack_Pos12_016.png image_D2_crack_Pos12_016.png visualization_D2_crack_Pos12_016.png \
-  visualization_D2_crack_Pos12_016.png -delay 20 -morph 10 -layers 'optimize' demo.gif
